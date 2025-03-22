ce5438f4061bbd5bb28cc6a675a7b729a205a1c9
Matthew Mettler <matthewrmettler@gmail.com>
2025-03-21 20:41:43 -0400

    Added tailwind and nextjs

---
+++ src/app/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// These styles apply to every route in the application
+import './globals.css';
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Page() {
+  return <h1 className="text-3xl font-bold text-amber-300 underline">Hello Next.js!</h1>;
+}
